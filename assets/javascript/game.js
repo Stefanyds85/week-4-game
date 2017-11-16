620bc00a785df0fa7560e562fd5700238e362777
Stefanyds85 <beautyxyzu4@aol.com>
2017-11-16 07:13:45 -0500

    commit js

---
+++ assets/javascript/game.js
@@ -0,0 +1,31 @@
+
+$(document).ready(function() {
+  var randomNumber = $("#randomNumber");
+  var compNumb =["min","max"];
+  var min = 19;
+  var max = 120; 
+            
+  function compNumb(min, max) {
+     return Math.floor() * (max - min) + min;
+      $("#randomNumber"). text(compNumb);
+    }
+
+
+var scoresList = ["Wins", "Losses"];
+var Wins = 0;
+var Losses = 0;
+
+var scoresList = $("#scores") 
+}
+
+
+$("#button1").on("click", function() {
+
+  Math.floor(Math.random() * 12) + 1;
+  return().text("#playerChoice")
+
+}
+
+
+    
+  });
